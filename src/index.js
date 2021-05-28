55784c921c31f55b814a5bb81128705aa6dd4370
Leonidas Lima <leondias.ollima@gmail.com>
2021-05-27 20:13:06 -0400

    Added cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 const express = require('express')
+const cors = require('cors')
 
 const emailRouter = require('./routers/email')
 
 const app = express()
 const port = process.env.PORT
+
+var whitelist = ['http://localhost:4200', 'https://reszon.com.br']
+var corsOptions = {
+    origin: function (origin, callback) {
+        console.log('origin: ', origin)
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
+app.use(cors(corsOptions))
 
 app.use(express.json())
 app.use(emailRouter)
